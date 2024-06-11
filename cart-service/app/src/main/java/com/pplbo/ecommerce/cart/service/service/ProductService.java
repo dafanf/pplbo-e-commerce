d7046bc57aa4c340a3fae07a5010ff29c23c512a
RaditPasya <radityaphr@gmail.com>
2024-06-11 22:53:33 +0700

    deu

--- cart-service/app/src/main/java/com/pplbo/ecommerce/cart/service/service/ProductService.java
+++ cart-service/app/src/main/java/com/pplbo/ecommerce/cart/service/service/ProductService.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.ResponseEntity;
 
 import com.pplbo.ecommerce.cart.service.dto.ProductRequest;
 import com.pplbo.ecommerce.cart.service.dto.ProductResponse;
@@ -11,9 +13,58 @@ import com.pplbo.ecommerce.cart.service.model.Product;
 import com.pplbo.ecommerce.cart.service.repository.ProductRepository;
 
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.Optional;
 
+@Service
 public class ProductService {
-    
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private final String PRODUCT_API_URL = "http://localhost:8085/api/product/";
+
+    public List<Product> getAllProducts() {
+        ResponseEntity<Product[]> response = restTemplate.getForEntity(PRODUCT_API_URL + "all", Product[].class);
+        return Arrays.asList(response.getBody());
+    }
+
+    public Optional<Product> getProductById(Long id) {
+        ResponseEntity<Product> response = restTemplate.getForEntity(PRODUCT_API_URL + id, Product.class);
+        return Optional.ofNullable(response.getBody());
+    }
+
+    public Product createProduct(Product product) {
+        return restTemplate.postForObject(PRODUCT_API_URL, product, Product.class);
+    }
+
+    public Product updateProduct(Long id, Product product) {
+        restTemplate.put(PRODUCT_API_URL + id, product);
+        return product;
+    }
+
+    public void deleteProduct(Long id) {
+        restTemplate.delete(PRODUCT_API_URL + id);
+    }
+
+
+    public List<Product> getAllProductsInCategory(Long categoryId) {
+        ResponseEntity<Product[]> response = restTemplate.getForEntity(PRODUCT_API_URL + "category/" + categoryId, Product[].class);
+        return Arrays.asList(response.getBody());
+    }
+
+    public Product createProductInCategory(Long categoryId, Product product) {
+        return restTemplate.postForObject(PRODUCT_API_URL + "category/" + categoryId, product, Product.class);
+    }
+
+    public Product updateProductInCategory(Long categoryId, Long productId, Product product) {
+        restTemplate.put(PRODUCT_API_URL + "category/" + categoryId + "/" + productId, product);
+        return product;
+    }
+
+    public void deleteProductInCategory(Long categoryId, Long productId) {
+        restTemplate.delete(PRODUCT_API_URL + "category/" + categoryId + "/" + productId);
+    }
 }
